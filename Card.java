14fbe8561fdeb71c9912a7411b8623c751ac99fb
sjnasr <sjnasr18@yahoo.com>
2015-04-01 14:48:53 -0700

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package poker_game_for_android;
+
+/**
+ *
+ * @author saralove
+ */
+public class Card {
+    
+}
