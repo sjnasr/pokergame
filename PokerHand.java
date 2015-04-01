fd685c90f4c619ffa318d718a5ca4c5222f66d76
sjnasr <sjnasr18@yahoo.com>
2015-04-01 14:50:04 -0700

    Create PokerHand.java

---
+++ PokerHand.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package poker_game_for_android;
+
+/**
+ *
+ * @author saralove
+ */
+public class PokerHand extends Deck {
+    
+    private Card [] hand;
+    
+}
